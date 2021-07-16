4b129dfb9a6d4746dd0163c58f3a486389c05579
Liav A <liavalb@gmail.com>
2021-07-16 19:15:36 +0300

    Core: Import TypedMapping code
    This code is from the SerenityOS project. I took it and adapted to my
    needs, so we don't really map virtual addresses to physical ones, but
    only allow a more comfortable coding style :^)

---
+++ Core/Memory/TypedMapping.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020, Andreas Kling <kling@serenityos.org>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#pragma once
+
+#include <Core/Memory/PhysicalAddress.h>
+
+namespace Core {
+
+template<typename T>
+struct TypedMapping {
+    const T* ptr() const { return reinterpret_cast<const T*>(m_base_address.offset(offset).as_ptr()); }
+    T* ptr() { return reinterpret_cast<T*>(m_base_address.offset(offset).as_ptr()); }
+    const T* operator->() const { return ptr(); }
+    T* operator->() { return ptr(); }
+    const T& operator*() const { return *ptr(); }
+    T& operator*() { return *ptr(); }
+    PhysicalAddress m_base_address;
+    size_t offset { 0 };
+};
+
+template<typename T>
+static TypedMapping<T> map_typed(PhysicalAddress paddr)
+{
+    TypedMapping<T> table;
+    table.offset = paddr.offset_in_page();
+    return table;
+}
+
+}
