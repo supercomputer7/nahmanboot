843975f48def24a7cc4f5afd1d4d5fb2e8ad772b
Liav A <liavalb@gmail.com>
2022-08-02 12:32:32 +0300

    Meta: Increase memory on test machine to 512 megabytes
    Also, append a basic test kernel commandline.

--- Meta/test.sh
+++ Meta/test.sh
@@ -1,3 +1,5 @@
 qemu-system-i386 -chardev stdio,id=stdout,mux=on \
+                 -m 512M \
                  -device isa-debugcon,chardev=stdout \
-                 -kernel ../Build/Core/image
+                 -kernel ../Build/Core/image \
+                 -append "hello"
